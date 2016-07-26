7f0c4ec9004038075618e3a9dc36e30bec1cda6e
rhythm003 <rhythm003@gmail.com>
2016-07-25 22:21:12 -0400

    inital commit for web api

---
+++ include/Config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "appadmin");
+define("DB_PASSWORD", "admin");
+define("DB_DATABASE", "android_api");
+?>
